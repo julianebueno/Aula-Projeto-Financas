36c6907d5448a6efc7f7b0962d5663c582499c6e
Juliane <julianebueno1@gmail.com>
2023-10-11 03:35:56 -0300

    refactor: :art: atualização do estilo global

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -16,6 +16,8 @@ body{
 
 button, input, textarea, select{
   outline: none;
+  border: none;
+  border-radius: 8px;
 }
 
 button, a{
@@ -26,6 +28,58 @@ a, li{
   text-decoration: none;
   list-style: none;
   color: inherit;
+}
+
+
+h2{
+  color: ${(props)=> props.theme.blue700};
+  font-family: 'Montserrat', sans-serif;
+  font-size: 38px;
+  font-weight: 700;
+}
+
+h3{
+  color: ${(props)=> props.theme.blue900};
+  font-family: 'Roboto', sans-serif;
+  font-size: 24px;
+  font-weight: 400;
+  line-height: normal;
+}
+
+p{
+  color: ${(props)=> props.theme.blue900};
+  font-family: 'Roboto', sans-serif;
+  font-size: 24px;
+  font-weight: 300;
+}
+
+a{
+  color: ${(props)=> props.theme.blue500};
+  font-family: 'Montserrat', sans-serif;
+  font-size: 24px;
+  font-weight: 400;
+}
+
+button{
+  display: flex;
+  align-items: center;
+  box-shadow: 5px 6px 4px 0px rgba(0, 0, 0, 0.25);
+  font-family: 'Roboto', sans-serif;
+  padding: 12px 48px;
+  font-size: 24px;
+  font-weight: 500;
+  background: ${(props)=> props.theme.blue700};
+  color: ${(props)=> props.theme.white50};
+}
+
+input{
+  padding: 8px 12px;
+  box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  height: 37px;
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  font-weight: 300;
+  background: ${(props)=> props.theme.white50};
 }
 
 `
